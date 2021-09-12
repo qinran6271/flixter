83b89f08fd87765f017cd46fd0555e45b9407388
qinran6271 <60624760+qinran6271@users.noreply.github.com>
2021-09-11 20:54:18 -0400

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Qinran Wang on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
